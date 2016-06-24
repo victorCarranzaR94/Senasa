4e27b08b5387c8243557d27475e6678f92938b5e
Harold <hjuarez20@gmail.com>
2016-06-24 17:36:17 -0600

    Harold 24/06/2016
    partes de lo fundamental

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
 <?php 
+require ("db/db.php");
 
+if($_POST){
+$data = $database->select("tbusuario", "*", [ "login" = $_POST["login"],
+                                              "password" => md5($_POST["password"])]);
 
+    if(count($data) == 1){
+       $_SESSION 
+    }
+}
 
 ?>
