12784adcff06b030e35b676c3b0ba63ea5d740bf
Victor Carranza Rodriguez <viccra3094@gmail.com>
2016-06-25 18:17:17 -0600

    Actualizacion de base de datos

--- Scripts Data Base/Script Senasa Beta 2.0.sql
+++ Scripts Data Base/Script Senasa Beta 2.0.sql
@@ -3,14 +3,14 @@ create database senasaCO;
 use senasaco;
 
 create table tbEnfermedad(
-codigo int primary key,
+codigo int primary key auto_increment,
 tipo varchar(20),
 nombre varchar(80)
 );
 
 
 create table tbCategoriaAnimal(
-codigo int primary key,
+codigo int primary key key auto_increment,
 nombre varchar(80)
 );
 
@@ -27,7 +27,7 @@ primary key(codEnfermedad,codOrden)
 );
 
 create table tbOrdenSanataria(
-codigo int primary key,
+codigo int primary key key auto_increment,
 lugar varchar(80),
 fecha date,
 imagen varchar(80),
@@ -36,20 +36,20 @@ codVisita int
 );
 
 create table tbControlMovilidad(
-codigo int primary key,
+codigo int primary key key auto_increment,
 tipo varchar(20),
 cantidad int,
 codVisita int
 );
 
 create table tbFiscalizacion(
-codigo int primary key,
+codigo int primary key key auto_increment,
 descripcion varchar(200),
 coidVisita int
 );
 
 create table tVisita(
-codigo int primary key,
+codigo int primary key key auto_increment,
 cantidadVisitas int,
 codHojaVisita int
 );
@@ -63,13 +63,13 @@ codUsuario int
 );
 
 create table tbUsuario(
-codigo int primary key,
+codigo int primary key key auto_increment,
 tipoUsuario varchar(80),
 login varchar(20),
-password varchar(20)
+password varchar(1000)
 );
 create table tbTarea(
-codigo int primary key,
+codigo int primary key key auto_increment,
 descripcion varchar(200),
 lugar varchar(200),
 fechaInicio date,
@@ -78,7 +78,7 @@ estado varchar(10)
 );
 
 create table tbCapacitacion(
-codigo int primary key,
+codigo int primary key key auto_increment,
 fecha date,
 lugar varchar(200),
 participantes int,
@@ -87,13 +87,13 @@ codHojaVisita int
 );
 
 create table tbTipoCapacitacion(
-codigo int primary key,
+codigo int primary key key auto_increment,
 evento varchar(200),
 cantidad  int
 );
 
 create table tbImportacionExportacion(
-codigo int primary key,
+codigo int primary key key auto_increment,
 numero int,
 especie varchar(200),
 codImportacion int,
@@ -102,7 +102,7 @@ codVisita int
 );
 
 create table tbOperacionSanitaria(
-codigo int primary key,
+codigo int primary key key auto_increment,
 fecha date,
 lugar varchar(200),
 cantidadAnimales int,
