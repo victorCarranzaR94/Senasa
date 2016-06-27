70782367a3b9239690d50352e4905bcdb130c55f
OscarUCR920 <oscargzsb@gmail.com>
2016-06-27 02:52:32 -0600

    Oscar 6/27/2016
    CAMBIOS CAMBIOS CAMBIOS

--- reunions.php
+++ reunions.php
@@ -26,16 +26,18 @@
                   
                     <form method="" action="">
                     <div id="form1">
+                        
+                        <label for="code" id="code">Código: </label>
+                        <input type="search" value="0" class="btnReg"></br>
 
                         <label for="date" id="date">Fecha: </label>
                         <input name="date" type="date" required placeholder="00/0/0000" class="prof">
                         
                         <label for="institution" id="institution">Institucion Organizadora: </label>
                          <input name="institution" type="text" required placeholder="Nombre de la Institucion" class="prof">
-                         <label for="asistanceAcount" id="asistanceAcount">Cantidad de Participantes: </label>
-                         <input name="asistanceAcount" type="text" required placeholder="0" class="prof">
                          
-                         <input type="submit" value="Guardar" class="btnReg">
+                         
+                         
                          
                          
                          
@@ -48,13 +50,15 @@
 
                          <label for="location" id="location">Ubicación: </label>
                          <input name="asistanceAcount" type="text" required placeholder="Dirección" class="prof">
+                         <label for="asistanceAcount" id="asistanceAcount">Cantidad de Participantes: </label>
+                         <input name="asistanceAcount" type="number" required placeholder="0" class="prof">
 
                     </div>
                     <br>
                     <br>
                     <div id="bottons">
                         <p>
-                            <input type="submit" value="Enviar" class="btnReg"> &nbsp; &nbsp;
+                            <input type="submit" value="Guardar" class="btnReg">&nbsp; &nbsp;
                             <input type="reset" name="reset" onClick="window.location.replace('principal.php')" value="Cancelar" class="btnReg">
                         </p>
                     </div>
