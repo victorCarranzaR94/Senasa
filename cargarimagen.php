62c6fc608c7d0fbf0cd2e164b7e56781f6e0aa0f
Harold <hjuarez20@gmail.com>
2016-06-27 02:32:38 -0600

    harold
    vista ingresar imagenes

--- cargarimagen.php
+++ cargarimagen.php
@@ -1,5 +1,6 @@
 <?php
-  require('headerAdmin.php');
+require('headerAdmin.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,20 +13,34 @@
         <link href="css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="js/fileinput.js" type="text/javascript"></script>
-        
+         <link rel="stylesheet" href="css/principal.css">
+         <link rel="stylesheet" href="css/profile.css" />
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+       <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+       <link href='https://fonts.googleapis.com/css?family=Roboto:400,900,700,500,300' rel='stylesheet' type='text/css'>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
 
 </head>
 <body>
+     <section id="home" class="medium">
+        
+    </section>
+    
      <div class="container kv-main">
+           <div id="titleProfile">Insertar Imagen</div>
+           <div>
+               <form>
+               <label for="codigo" id="codigo">Codigo de Hoja de Visita: </label>
+               <input name="codigo" type="text" required >
+               <p><input type="submit" value="Buscar" class="btnReg"> &nbsp; &nbsp;</p>
+               </form>    
+           </div>
+                
             <form enctype="multipart/form-data">
                 <div class="form-group">
                     <input id="file-5" class="file" type="file" multiple data-preview-file-type="any" data-upload-url="#">
                 </div>
-                
-                <div>
-                    
-                </div>
+                <p><input type="submit" value="Guardar" class="btnReg"> &nbsp; &nbsp;</p>
             </form>
             
         </div>
